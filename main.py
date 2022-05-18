b23ea12c9f2d148a576d8de34f74e7ba44e92bc2
bhaskarreddy <bhaskrrd10@gmail.com>
2022-05-18 14:58:52 +0530

    added buutons

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Pfrogram")
 window.minsize(width=500, height=300)
 
 #label
 
-my_label = tkinter.Label(text="I am a Label",font=("Ariel", 24,"italic"))
+my_label = Label(text="I am a Label",font=("Ariel", 24,"italic"))
 my_label.pack(side="left")
 
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
 
+
+button = Button(text="Click Me")
 
 
 window.mainloop()
